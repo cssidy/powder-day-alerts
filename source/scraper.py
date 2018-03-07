44c97795674b4bb8bc31ccc57630d1deaef51857
cssidy <cassidy@cassidybrooke.net>
2018-03-07 15:23:09 -0500

    updating scraper function

--- source/scraper.py
+++ source/scraper.py
@@ -3,7 +3,7 @@
 
 import bs4
 import requests
-import datetime
+from twilio.rest import Client
 
 mountain_list = ['Stowe+Mountain+Resort',
                  'Smugglers+Notch',
@@ -24,20 +24,49 @@ def scraper():
         res = requests.get('https://www.google.com/search?ei=JiOgWtGbBIeSsAWZmpvAAg&q=%+snow+report' % (mountain))
         res.raise_for_status()
 
-        soup = bs4.BeautifulSoup(res.text, "lxml")
+        soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
         # get current snow report for last 24 hours
         print('Searching for recent snowfall at %s...' % (mountain))
-        element = soup.select('#resultStats')
-        element = element[0].text
+        element = soup.find_all(text="24 Hours")
+        # element = element[15]
         print(element)
+        print('\n')
 
-        now = datetime.datetime.now()
+        '''
 
-        if element == []:
+
+        if not element:
             print('Could not find recent snow fall for %s.' % (mountain))
         else:
             print('Snowfall: ' + element)
+
+            '''
+
+        '''
+        def send_text():
+            if element >= 6:
+
+                # send text with Twilio
+                account_sid = "YOUR_ACCOUNT_SID"
+                auth_token = "YOUR_AUTH_TOKEN"
+                client = Client(account_sid, auth_token)
+
+                client.api.account.messages.create(
+
+                    to="YOUR_PHONE",
+                    from_="YOUR_TWILIO_NUMBER",
+                    body=("It snowed " + element + 'inches at %s in the past 24 hours.' % (mountain))
+                )
+
+                print('Just sent a text message...')
+            else:
+                pass
+
+        send_text()
+        '''
+
+
 
 
 scraper()
